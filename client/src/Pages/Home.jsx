52725b4be2262394d214f1ed5ce11e5a9850c342
Dinesh Prajapati <dineshprj7@gmail.com>
2026-05-05 22:07:52 +0530

    First day of Resume Builder

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Banner from './Components/Home/Banner'
+import Hero from './Components/Home/Hero'
+import Feautes from './Components/Home/Feautes'
+
+const Home = () => {
+  return (
+    <div>
+        <Banner />
+        <Hero />
+        <Feautes />
+
+    </div>
+  )
+}
+
+export default Home
